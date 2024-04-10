48378099eb630fcab6bacd9a269106e22796f5ed
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-04-10 20:44:36 -0300

    Adicionando arquivos não adicionados antes por falha do git

--- Forca/Forca.c
+++ Forca/Forca.c
@@ -3,34 +3,26 @@
 #include<stdlib.h>
 #include<time.h>
 
-void verifica(FILE*p)
+void verifica(FILE**p, char arquivo[20])
 {
-    if(p==NULL)
+    *p = fopen(arquivo, "r");
+    if(*p==NULL)
     {
         puts("Não foi possivel acessar o arquivo");
-    }
-}
-
-char* abr_arquivo(FILE*p, char nome_arquivo[15], char palavra[15])
-{
-    int tamanho;
-    fscanf(p,"%d", &tamanho);
-    srand(time(0));
-    int conta = rand() % tamanho;
-
-    for (int i = 0; i <= conta; i++)
-    {
-        fscanf(p,nome_arquivo,palavra);
+        return 1;
     }
 
-    return palavra;
+    
 }
 
 void escolhe_palavra(char palavras[15])
 {
     //Espaço para variaveis
+
     int tdp=0;
+
     // Mensagem de abertura
+
     puts("Escolha o tipo de palavra que quer jogar!");
     puts("");
     puts("Para PROFISSÃO tecle (1); FRUTAS (2); Estados(3)");
@@ -42,10 +34,9 @@ void escolhe_palavra(char palavras[15])
     {
         case 1:
         {
-            FILE* p = fopen("profissões.txt", "r");
-            verifica(p);
+            FILE* p;
+            verifica(&p, "profissões.txt");
 
-            abr_arquivo(p, "profissões.txt", palavras);
 
         }
     }
@@ -55,33 +46,6 @@ int main(){
 
     char palavra_secreta[15];
     escolhe_palavra(palavra_secreta);
-
-  //  int ganhou = 0;
-   // int enforcou = 0;
-
-   // int tentativas = 0;
-  //  char chutes[26];
-
-    // do{   
-    //     int tem = 0;
-    //     char chute;
-    //     scanf(" %c", &chute);
-
-    //     for (int i = 0; i < strlen(palavrasecreta); i++){
-            
-    //         for (int j = 0; j < tentativas; j++){
-    //             chutes[i] = chute;
-    //             if(chutes[j] == palavrasecreta[i]){
-    //                 tem = 1;
-    //             }
-                
-    //         }
-            
-    //     }
-        
-    
-            
-    // }while (!ganhou && !enforcou);
 
     return 0;
 }
